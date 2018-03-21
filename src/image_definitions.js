b3e9c58d2b134dcc4fdcb7b0483aec955aed9d60
Sigve Indregard <sigve@indregard.no>
2018-03-21 16:54:48 +0100

    Image definitions

--- src/image_definitions.js
+++ src/image_definitions.js
@@ -1,5 +1,13 @@
 import ferret from './images/ferret.png';
 import skog from './images/branch.png';
+import fridge from './images/fridge.png';
+import yell from './images/yell.png';
+import suitcase from './images/suitcase.png';
+import camera from './images/camera.png';
+import cow from './images/cow.png';
+import cards from './images/cards.png';
+import sex from './images/sex.png';
+import car from './images/car.png';
 
 var images = {
 	"ferret": {
@@ -12,6 +20,46 @@ var images = {
 		"image": skog,
 		"credit": "Creative Commons: Branch av Matthew Brinkley (Noun Project)"
 	},
+  "fridge": {
+    "alt": "Et kjøleskap",
+    "image": fridge,
+    "credit": "Creative Commons: Fridge av Vectors Market (Noun Project)"
+  },
+  "yell": {
+    "alt": "En krangel",
+    "image": yell,
+    "credit": "Creative Commons: James Keuning"
+  },
+  "suitcase": {
+    "alt": "En koffert",
+    "image": suitcase,
+    "credit": "Creative Commons: Suitcase av Peter Carleton (Noun Project)"
+  },
+  "camera": {
+    "alt": "Et overvåkingskamera",
+    "image": camera,
+    "credit": "Creative Commons: Surveillance av Lee (Noun Project)"
+  },
+  "cow": {
+    "alt": "Ei ku",
+    "image": cow,
+    "credit": "Creative Commons: Cow av iconsmind.com (Noun Project)"
+  },
+  "cards": {
+    "alt": "Et menneske spiller kort",
+    "image": cards,
+    "credit": "Andy Santos-Johnson (Noun Project)"
+  },
+  "sex": {
+    "alt": "Håndsymbol for sex",
+    "image": sex,
+    "credit": "Creative Commons: Sex av sobinsergey (Noun Project)"
+  },
+  "car": {
+    "alt": "En bil",
+    "image": car,
+    "credit": "Creative Commons: Car av Oeda (Noun Project)"
+  }
 }
 
 export default images;
