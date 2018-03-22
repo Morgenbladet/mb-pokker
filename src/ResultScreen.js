a68632257d0c2663e10d6038ea6c6c4d56e26a67
Sigve Indregard <sigve@indregard.no>
2018-03-22 12:52:54 +0100

    Draw randomly among locations and occupations

--- src/ResultScreen.js
+++ src/ResultScreen.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import shuffleArray from './shuffle_array.js';
+import geo_definitions from './geo_definitions.json';
+import edu_defs from './edu_definitions.json';
 
 class ResultScreen extends Component {
 	constructor(props) {
@@ -42,27 +45,28 @@ class ResultScreen extends Component {
 	}
 
 	geo() {
-		let range = 71-57;
-		let pos = 57 + range * (this.props.geo / 100.0);
-
-		return "" + pos + ". breddegrad";
+    let geo = Math.round(this.props.geo / 10.0); //should be in [0,10]
+    return shuffleArray(geo_definitions[geo])[0];
 	}
 
 	edu() {
 		let edu = this.props.edu;
+    let alts = null;
 		if (edu < 10) {
-			return "utdanning på barneskolenivå";
+      alts = edu_defs[0];
 		} else if (edu < 30) {
-			return "fagbrev";
+      alts = edu_defs[1];
 		} else if (edu < 50) {
-			return "bachelor fra Oslomet";
+      alts = edu_defs[2];
 		} else if (edu < 70) {
-			return "bachelor fra Blindern";
+      alts = edu_defs[3];
 		} else if (edu < 90) {
-			return "mastergrad";
+      alts = edu_defs[4];
 		} else {
-			return "minst én doktorgrad"
+      alts = edu_defs[5];
 		}
+
+    return shuffleArray(alts)[0];
 	}
 
 	render() {
@@ -79,12 +83,13 @@ class ResultScreen extends Component {
 
 		let results = [
 			<Result key="1" prefix="en " variable={ this.age() } suffix=" år gammel" />,
-			<Result key="2" variable={ this.sex() } />,
-			<Result key="3" prefix="fra " variable={ this.geo() } />,
-			<Result key="4" prefix="med " variable={ this.edu() } />
+			<Result key="2" prefix="" variable={ this.edu() } suffix=". "/>,
+			<Result key="3" prefix="Du er " variable={ this.sex() } />,
+			<Result key="4" prefix="og kommer fra " variable={ this.geo() } suffix="."/>
 		]
 		return(
 			<div className="ResultScreen">
+        <h1>Dette er deg</h1>
 				<p>Du banner som en { results }.</p>
 			</div>
 		);
